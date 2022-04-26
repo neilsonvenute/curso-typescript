be9f64a24668b6889849fb8f855b990fb75bd7a0
Neilson Venute <neilsonvenute@gmail.com>
2022-04-26 19:25:31 -0300

    tipo any

---
+++ src/A0001-type-annotation/A0002-type-any.ts
@@ -0,0 +1,9 @@
+//utilize any em apenas em ultimo caso
+
+function showMessage(msg: any) {
+  return msg;
+}
+
+console.log(showMessage([1, 2, 3]));
+console.log(showMessage('Olá'));
+console.log(showMessage(1));
