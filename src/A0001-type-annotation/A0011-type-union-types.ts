cb5675d766e33d877f489f2e02d00f8958a3d1f9
Neilson Venute <neilsonvenute@gmail.com>
2022-04-27 18:53:21 -0300

    tipo union types

---
+++ src/A0001-type-annotation/A0011-type-union-types.ts
@@ -0,0 +1,7 @@
+function addConcat(a: number | string, b: number | string) {
+  if (typeof a === 'number' && typeof b === 'number') return a + b;
+  return `${a}${b}`;
+}
+
+console.log(addConcat(10, 20));
+console.log(addConcat('10', '20'));
