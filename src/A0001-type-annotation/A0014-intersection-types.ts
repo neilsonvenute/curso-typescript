9249ac178af3b6d2d738ebeaab7156335d9e1d2e
Neilson Venute <neilsonvenute@gmail.com>
2022-04-27 20:29:50 -0300

    type intersection-types

--- src/A0001-type-annotation/A0014-intersection-types.ts
+++ src/A0001-type-annotation/A0014-intersection-types.ts
@@ -3,9 +3,9 @@ type TemSobrenome = { sobrenome: string };
 type TemIdade = { idade: number };
 type Pessoa = TemNome & TemSobrenome & TemIdade;
 
-type AB = 'A' | 'B';
-type AC = 'A' | 'C';
-type AD = 'D' | 'A';
+//type AB = 'A' | 'B';
+//type AC = 'A' | 'C';
+//type AD = 'D' | 'A';
 //type intersecao = AB & AC & AD;
 
 const pessoa: Pessoa = {
