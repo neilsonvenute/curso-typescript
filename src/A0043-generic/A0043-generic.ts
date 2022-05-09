002d5a6768632b0182e8348aa32c422eff344a9e
Neilson Venute <neilsonvenute@gmail.com>
2022-05-08 21:38:07 -0300

    generic

---
+++ src/A0043-generic/A0043-generic.ts
@@ -0,0 +1,20 @@
+type FilterCallBack<U> = (value: U, index?: number, array?: U[]) => boolean;
+
+export function meuFilter<T>(array: T[], callbackfn: FilterCallBack<T>): T[] {
+  const novoArray: T[] = [];
+
+  for (let i = 0; i < array.length; i++) {
+    if (callbackfn(array[i])) {
+      novoArray.push(array[i]);
+    }
+  }
+  return novoArray;
+}
+
+const array = [1, 2, 3, 5, 6, 7, 8, 9, 10];
+
+const arrayFiltradoOriginal = array.filter((value) => value < 5);
+console.log(arrayFiltradoOriginal);
+
+const arrayFiltrado = meuFilter(array, (value) => value < 5);
+console.log(arrayFiltrado);
