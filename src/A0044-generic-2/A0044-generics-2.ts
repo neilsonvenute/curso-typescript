4cb0b3fa2d5b4b1fec5c816426f1626ee54df845
Neilson Venute <neilsonvenute@gmail.com>
2022-05-11 19:59:59 -0300

    generic 2

---
+++ src/A0044-generic-2/A0044-generics-2.ts
@@ -0,0 +1,18 @@
+const arrayNumeros: Array<number> = [1, 2, 3, 4, 5, 6];
+console.log(arrayNumeros);
+
+async function promiseAsync() {
+  return 1;
+}
+
+function minhaPromise(): Promise<number> {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(1);
+    }, 1000);
+  });
+}
+
+promiseAsync().then((resultado) => console.log(resultado + 1));
+
+minhaPromise().then((resultado) => console.log(resultado + 1));
