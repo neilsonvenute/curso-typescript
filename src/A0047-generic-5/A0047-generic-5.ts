5cae2fc25c8cd939a2c2c1f2ab8dfd92cd99c029
Neilson Venute <neilsonvenute@gmail.com>
2022-05-12 19:51:36 -0300

    generic 4 - generic com classes

---
+++ src/A0047-generic-5/A0047-generic-5.ts
@@ -0,0 +1,49 @@
+export class Pessoa<T, U> {
+  constructor(public nome: T, public idade: U) {}
+}
+
+export class Pilha<T> {
+  private contador = 0;
+  private elementos: { [K: number]: T } = {};
+
+  push(elemento: T): void {
+    this.elementos[this.contador] = elemento;
+    this.contador++;
+  }
+
+  pop(): T | void {
+    if (this.estaVazia()) return undefined;
+
+    this.contador--;
+    const elemento = this.elementos[this.contador];
+    return elemento;
+  }
+
+  estaVazia(): boolean {
+    return this.contador == 0;
+  }
+
+  tamanho(): number {
+    return this.contador;
+  }
+
+  mostrarPilha(): void {
+    for (const chave in this.elementos) {
+      console.log(this.elementos[chave]);
+    }
+  }
+}
+
+const pilha = new Pilha<number>();
+pilha.push(1);
+pilha.push(2);
+pilha.push(3);
+pilha.push(4);
+pilha.mostrarPilha();
+const elemento1 = pilha.pop();
+pilha.mostrarPilha();
+console.log(elemento1);
+
+while (!pilha.estaVazia()) {
+  console.log(pilha.pop());
+}
